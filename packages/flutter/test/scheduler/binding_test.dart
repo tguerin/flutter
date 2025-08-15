1fe4f44aa5e3fc3d3e5aa62206631868542d44ee
tguerin <contact@7omtech.fr>
2025-08-15 12:17:22 +0200

    Allow to force frames rendering

--- packages/flutter/test/scheduler/binding_test.dart
+++ packages/flutter/test/scheduler/binding_test.dart
@@ -45,4 +45,95 @@ void main() {
     SchedulerBinding.instance.handleDrawFrame();
     expect(calledBack, true);
   });
+
+  group('forceFramesEnabled', () {
+    setUp(() {
+      WidgetsBinding.instance.attachRootWidget(const SizedBox.shrink());
+      SchedulerBinding.instance.resetInternalState();
+    });
+
+    tearDown(() {
+      SchedulerBinding.instance.resetInternalState();
+    });
+
+    test('defaults to false and uses normal lifecycle behavior', () {
+      expect(SchedulerBinding.instance.forceFramesEnabled, isFalse);
+      expect(SchedulerBinding.instance.framesEnabled, isTrue);
+
+      WidgetsBinding.instance.handleAppLifecycleStateChanged(AppLifecycleState.hidden);
+      expect(SchedulerBinding.instance.framesEnabled, isFalse);
+
+      WidgetsBinding.instance.handleAppLifecycleStateChanged(AppLifecycleState.resumed);
+      expect(SchedulerBinding.instance.framesEnabled, isTrue);
+    });
+
+    test('when set to true, keeps frames enabled when hidden or paused but not when detached', () {
+      SchedulerBinding.instance.forceFramesEnabled = true;
+      expect(SchedulerBinding.instance.framesEnabled, isTrue);
+
+      WidgetsBinding.instance.handleAppLifecycleStateChanged(AppLifecycleState.hidden);
+      expect(SchedulerBinding.instance.framesEnabled, isTrue);
+
+      WidgetsBinding.instance.handleAppLifecycleStateChanged(AppLifecycleState.paused);
+      expect(SchedulerBinding.instance.framesEnabled, isTrue);
+
+      WidgetsBinding.instance.handleAppLifecycleStateChanged(AppLifecycleState.detached);
+      expect(SchedulerBinding.instance.framesEnabled, isFalse);
+    });
+
+    test('does not disable frames when app is active', () {
+      SchedulerBinding.instance.forceFramesEnabled = false;
+      expect(SchedulerBinding.instance.framesEnabled, isTrue);
+
+      WidgetsBinding.instance.handleAppLifecycleStateChanged(AppLifecycleState.resumed);
+      expect(SchedulerBinding.instance.framesEnabled, isTrue);
+
+      WidgetsBinding.instance.handleAppLifecycleStateChanged(AppLifecycleState.inactive);
+      expect(SchedulerBinding.instance.framesEnabled, isTrue);
+    });
+
+    test('can be toggled on and off', () {
+      expect(SchedulerBinding.instance.forceFramesEnabled, isFalse);
+      WidgetsBinding.instance.handleAppLifecycleStateChanged(AppLifecycleState.hidden);
+      expect(SchedulerBinding.instance.framesEnabled, isFalse);
+
+      SchedulerBinding.instance.forceFramesEnabled = true;
+      expect(SchedulerBinding.instance.framesEnabled, isTrue);
+
+      SchedulerBinding.instance.forceFramesEnabled = false;
+      expect(SchedulerBinding.instance.framesEnabled, isFalse);
+
+      WidgetsBinding.instance.handleAppLifecycleStateChanged(AppLifecycleState.resumed);
+      expect(SchedulerBinding.instance.framesEnabled, isTrue);
+    });
+
+    test('schedules frame when changed from false to true while app is hidden', () {
+      SchedulerBinding.instance.handleBeginFrame(null);
+      WidgetsBinding.instance.handleAppLifecycleStateChanged(AppLifecycleState.hidden);
+      expect(SchedulerBinding.instance.framesEnabled, isFalse);
+      expect(SchedulerBinding.instance.hasScheduledFrame, isFalse);
+
+      SchedulerBinding.instance.forceFramesEnabled = true;
+      expect(SchedulerBinding.instance.framesEnabled, isTrue);
+      expect(SchedulerBinding.instance.hasScheduledFrame, isTrue);
+    });
+
+    test('never enables frames when detached, even when forced', () {
+      SchedulerBinding.instance.forceFramesEnabled = true;
+      expect(SchedulerBinding.instance.framesEnabled, isTrue);
+
+      WidgetsBinding.instance.handleAppLifecycleStateChanged(AppLifecycleState.detached);
+      expect(SchedulerBinding.instance.framesEnabled, isFalse);
+
+      expect(SchedulerBinding.instance.forceFramesEnabled, isTrue);
+    });
+
+    test('resetInternalState clears force flag', () {
+      SchedulerBinding.instance.forceFramesEnabled = true;
+      expect(SchedulerBinding.instance.forceFramesEnabled, isTrue);
+
+      SchedulerBinding.instance.resetInternalState();
+      expect(SchedulerBinding.instance.forceFramesEnabled, isFalse);
+    });
+  });
 }
